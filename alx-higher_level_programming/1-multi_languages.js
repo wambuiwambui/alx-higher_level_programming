4a4de1a7a9c9a23ca55654afb0178bdb14bee629
wambuiwambui <tnjoki11@gmail.com>
2023-03-13 14:39:03 +0300

    all done

---
+++ alx-higher_level_programming/1-multi_languages.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+console.log(“C is fun”);
+console.log(“Python is cool”“P);
+console.log(“JavaScript is amazing”)
---
+++ alx-higher_level_programming/2-arguments.js
@@ -0,0 +1,6 @@
+if argument === Null;
+console.log(“No argument”);
+elif argument === 1;
+console.log(“Argument found”);
+else;
+console.log(“Arguments found”)
