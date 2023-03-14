b2d167be40b05b4f6f3f1deda1cb05f45c4989b5
wambuiwambui <tnjoki11@gmail.com>
2023-03-15 00:57:50 +0300

    all done

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,19 +1,26 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-	constructor(w, h) {
-		if(w <= 0 || h <= 0){
-			this.height = 0;
-			this.width = 0;
-		}else{
-			this.height = h;
-			this.width = w;
-			{
-				print(){
-					for(let x = 0; this.height; x++) {
-						console.log("x".repeat(this.width));
-					}
-				}
-			}
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
