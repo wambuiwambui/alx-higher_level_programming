cc1bfb3bc5de4c2c46292fd09729d5cb33a3364f
wambuiwambui <tnjoki11@gmail.com>
2023-03-22 17:47:31 +0300

    Initial commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-
-"""
-python script that lists all states from the database hbtn_0e_0_usa
-"""
-
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        # Open database connection
+        db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor()
 
-    for row in rows:
-        print(row)
+        # execute SQL query using execute() method.
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    cursor.close()
-    db.close()
+        # Fetch a single row using fetchone() method.
+        rows = cursor.fetchall()
+
+        # Iterate through the rows and print the results
+        for row in rows:
+            print(row)
+
+        # disconnect from server
+        db.close()
+    else:
+        print("Usage: {} username password database".format(sys.argv[0]))
